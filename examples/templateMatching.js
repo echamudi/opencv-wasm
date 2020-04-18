be76c7486f39593d209cd8e85fff354a3cc4d2e8
Ezzat Chamudi <chamudi.git@outlook.com>
2020-04-19 02:48:40 +0300

    Add error handling

--- examples/templateMatching.js
+++ examples/templateMatching.js
@@ -1,7 +1,7 @@
 const { Canvas, createCanvas, Image, ImageData, loadImage } = require('canvas');
 const { JSDOM } = require('jsdom');
 const { writeFileSync } = require('fs');
-const cv = require('../');
+const { cv, cvErrorPrinter } = require('../');
 
 function installDOM() {
     const dom = new JSDOM();
@@ -48,24 +48,6 @@ function installDOM() {
         writeFileSync('temp.png', canvas.toBuffer('image/png'));
 
     } catch (err) {
-        if (typeof err === 'undefined') {
-            err = '';
-        } else if (typeof err === 'number') {
-            if (!isNaN(err)) {
-                if (typeof cv !== 'undefined') {
-                    err = 'Exception: ' + cv.exceptionFromPtr(err).msg;
-                }
-            }
-        } else if (typeof err === 'string') {
-            let ptr = Number(err.split(' ')[0]);
-            if (!isNaN(ptr)) {
-                if (typeof cv !== 'undefined') {
-                    err = 'Exception: ' + cv.exceptionFromPtr(ptr).msg;
-                }
-            }
-        } else if (err instanceof Error) {
-            err = err.stack.replace(/\n/g, '<br>');
-        }
-        console.log(err);
+        console.log(cvErrorPrinter(err, cv));
     }
 })();
