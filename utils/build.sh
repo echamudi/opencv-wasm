19006cdce8b7c6d8d6eb0f6de4c6ebb46a19e84c
Ezzat Chamudi <chamudi.git@outlook.com>
2020-04-19 00:14:01 +0300

    Move and add utilities

--- utils/build.sh
+++ utils/build.sh
@@ -3,13 +3,17 @@ cd opencv
 git checkout 4.3.0
 
 # Add non async flag before compiling in the python build_js.py script
-#         flags = "-s WASM_ASYNC_COMPILATION=0 "
+#         flags += "-s WASM_ASYNC_COMPILATION=0 "
+#         flags += "-s SINGLE_FILE=0 "
 docker run --rm --workdir /code -v "$PWD":/code "trzeci/emscripten:sdk-tag-1.38.32-64bit" python ./platforms/js/build_js.py build_wasm --build_wasm --build_test
 
-# test (use node carbon)
+# before continuing, beautify the js using js-beautify and separate the wasm file using seperateBinaryFile.js
+
+# test (use node LTS)
 cd build_wasm/bin
 npm install
 node test.js
 
 cd ../../..
 cp ./opencv/build_wasm/bin/opencv.js .
+
