da23b9834e1a56a360fc633df65cc50bc6273265
Ezzat Chamudi <chamudi.git@outlook.com>
2020-04-18 19:36:58 +0300

    Update flags guide

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 git clone https://github.com/opencv/opencv.git
 cd opencv
 git checkout 4.3.0
+
+# Add non async flag before compiling in the python build_js.py script
+#         flags = "-s WASM_ASYNC_COMPILATION=0 "
 docker run --rm --workdir /code -v "$PWD":/code "trzeci/emscripten:sdk-tag-1.38.32-64bit" python ./platforms/js/build_js.py build_wasm --build_wasm --build_test
 
 # test (use node carbon)
