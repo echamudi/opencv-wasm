7d999e4863a7be49daaa871af624ed5e9025d2a1
Ezzat Chamudi <chamudi.git@outlook.com>
2020-04-28 03:27:53 +0300

    Update chai to assert style

--- test/test.js
+++ test/test.js
@@ -1,11 +1,10 @@
 const { cv, cvTranslateError } = require('../');
-const assert = require('assert');
 const path = require('path');
-const chai = require('chai');
+const { assert } = require('chai');
 
-describe('opencv-wasm', function() {
-    it('can be loaded', function() {
-        chai.expect(cv).to.be.an('object');
-        chai.expect(cvTranslateError).to.be.a('function');
+describe('opencv-wasm', function () {
+    it('can be loaded', function () {
+        assert.isObject(cv);
+        assert.isFunction(cvTranslateError);
     });
 });
