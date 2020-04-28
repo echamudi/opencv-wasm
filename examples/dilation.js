e9071942e02fa41406a46c4dbae007c8f65465bf
Ezzat Chamudi <chamudi.git@outlook.com>
2020-04-28 04:18:08 +0300

    Format document

--- examples/dilation.js
+++ examples/dilation.js
@@ -7,19 +7,16 @@ const { cv } = require('../');
     let jimpSrc = await Jimp.read(__dirname + '/input/image-sample-1.jpg');
 
     var src = cv.matFromImageData(jimpSrc.bitmap);
-    
+
     let dst = new cv.Mat();
     let M = cv.Mat.ones(5, 5, cv.CV_8U);
     let anchor = new cv.Point(-1, -1);
     cv.dilate(src, dst, M, anchor, 1, cv.BORDER_CONSTANT, cv.morphologyDefaultBorderValue());
-    
+
     new Jimp({
         width: dst.cols,
         height: dst.rows,
         data: Buffer.from(dst.data)
     })
-
-    .write(__dirname + '/test-output/dilation.png');
-    src.delete();
-    dst.delete();
+        .write(__dirname + '/test-output/dilation.png');
 })();
