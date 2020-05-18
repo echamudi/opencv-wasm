4b89b5668323c32f9ddaebd0c877ceba0866555a
Ezzat Chamudi <chamudi.git@outlook.com>
2020-05-19 00:27:37 +0300

    Update generator file

--- utils/generateCvProps.js
+++ utils/generateCvProps.js
@@ -9,6 +9,10 @@ const removeProps = [
 let keys = Object.keys(cv);
 
 let result = '';
+result += `// Generated types by generateCvProps.js, don't edit this file\n`;
+result += `\n`;
+result += `\n`;
+
 keys.forEach(key => {
     if (removeProps.indexOf(key) !== -1) return;
 
@@ -18,12 +22,17 @@ keys.forEach(key => {
     if (type === 'string' || type === 'number' || type === 'boolean') {
         result += `export var ${key}: ${type};\n`;
     } else if (type === 'function') {
-        console.log(key);
-        result += `export var ${key}: any; // ${type}\n`;
+        result += `\n`;
+        result += `/** function */\n`;
+        result += `export var ${key}: any;\n`;
     } else if (type === 'object') {
-        result += `export var ${key}: any; // ${type}\n`;
+        result += `\n`;
+        result += `/** object */\n`;
+        result += `export var ${key}: any;\n`;
     } else if (type === 'undefined') {
-        result += `export var ${key}: any; // ${type}\n`;
+        result += `\n`;
+        result += `/** undefined */\n`;
+        result += `export var ${key}: any;\n`;
     } else {
         throw new Error(key + ' ' + type);
     }
