c2ff347167a40225102d73f086e8eca4563ff1b3
Ezzat Chamudi <echamudi@outlook.com>
2020-12-07 00:36:34 +0300

    Update build script

--- utils/build.sh
+++ utils/build.sh
@@ -1,39 +1,29 @@
 git clone --branch 4.3.0 --depth 1 https://github.com/opencv/opencv.git
-cd opencv
-git checkout 4.3.0
 
-# Add non async flag before compiling in the python build_js.py script
-docker run --rm --workdir /code -v "$PWD":/code "trzeci/emscripten:sdk-tag-1.38.32-64bit" python ./platforms/js/build_js.py build_wasm --build_wasm --build_test --build_flags "-s WASM=1 -s WASM_ASYNC_COMPILATION=0 -s SINGLE_FILE=0 "
+# Build
+(
+    cd opencv &&
+    git checkout 4.3.0 &&
 
-# Backup the default build_wasm resukt
-cp -a ./build_wasm/ ./build_wasm_backup
-find ./build_wasm/ -type d -exec chmod 755 {} \;
-find ./build_wasm/ -type f -exec chmod 644 {} \;
+    # Add non async flag before compiling in the python build_js.py script
+    docker run --rm --workdir /code -v "$PWD":/code "trzeci/emscripten:sdk-tag-1.38.32-64bit" python ./platforms/js/build_js.py build_wasm --build_wasm --build_test --build_flags "-s WASM=1 -s WASM_ASYNC_COMPILATION=0 -s SINGLE_FILE=0 "
+)
 
-# separate wasm
-cd ../
+# Copy compilatio result
+cp -a ./opencv/build_wasm/ ./build_wasm
+
+# Create Wasm file
 node seperateBinaryFile.js
-cd opencv/build_wasm/bin
 
-# beautify opencv.js using js-beautify
-npx js-beautify opencv.js -r
+# Beautify JS
+(
+    cd ./build_wasm/bin &&
+    npx js-beautify opencv.js -r
+)
 
-# Test using built in test
-npm install
-node tests.js # make sure 0 failed
-
-# copy results to root
-cd ../../..
-cp ./opencv/build_wasm/bin/opencv.wasm ../opencv.wasm
-cp ./opencv/build_wasm/bin/opencv.js ../opencv.js
-
-# cp ./opencv/build_wasm/bin/haarcascade_frontalface_default.xml .
-# cp ./opencv/build_wasm/bin/test_calib3d.js .
-# cp ./opencv/build_wasm/bin/test_features2d.js .
-# cp ./opencv/build_wasm/bin/test_imgproc.js .
-# cp ./opencv/build_wasm/bin/test_mat.js .
-# cp ./opencv/build_wasm/bin/test_objdetect.js .
-# cp ./opencv/build_wasm/bin/test_photo.js .
-# cp ./opencv/build_wasm/bin/test_utils.js .
-# cp ./opencv/build_wasm/bin/test_video.js .
-# cp ./opencv/build_wasm/bin/tests.js .
+# Copy bins to root
+(
+    cp ./build_wasm/bin/opencv.wasm ../opencv.wasm &&
+    cp ./build_wasm/bin/opencv.js ../opencv.js &&
+    cp -r ./build_wasm/ ../build_wasm_test
+)
--- utils/seperateBinaryFile.js
+++ utils/seperateBinaryFile.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
+const path = require('path');
 
-const openCvJs = fs.readFileSync('./opencv/build_wasm/bin/opencv.js', 'utf-8');
+const openCvJs = fs.readFileSync(path.join(__dirname, './build_wasm/bin/opencv.js'), 'utf-8');
 
 // Generate OpenCV.wasm
 const matchResult = openCvJs.match(/var wasmBinaryFile="(.*?)";/gs);
@@ -11,10 +12,10 @@ if (matchResult == null) {
 
 const wasmBase64 = matchResult[0].replace('var wasmBinaryFile="data:application/octet-stream;base64,', '').replace('=";', '');
 
-fs.writeFileSync('./opencv/build_wasm/bin/opencv.wasm', wasmBase64, {encoding: 'base64'});
+fs.writeFileSync(path.join(__dirname, './build_wasm/bin/opencv.wasm'), wasmBase64, {encoding: 'base64'});
 console.log('Generated OpenCV.wasm');
 
 // Update opencv.js
 const openCvJsNew = openCvJs.replace(/var wasmBinaryFile="(.*?)";/, 'var wasmBinaryFile="./opencv.wasm";');
-fs.writeFileSync('./opencv/build_wasm/bin/opencv.js', openCvJsNew, {encoding: 'utf8'});
+fs.writeFileSync(path.join(__dirname, './build_wasm/bin/opencv.js'), openCvJsNew, {encoding: 'utf8'});
 console.log('Updated opencv.js');
